59198892fd1f0a674f88503d4b6fc95197c37fa5
Geraldo Moreira <gera.moreira11@gmail.com>
2024-09-09 11:37:38 -0300

    adc commit correto ao projeto bank

--- bank_system.py
+++ bank_system.py
@@ -1,29 +1,76 @@
 '''
-nome = "geraldo"
-idade = 33
-nome = "caio"
-print(nome, idade)
+Sistema bancário simples:
 
-BRAZILIAN_STATES =["SP", "RJ", "SC", "RS"]
+Operações: Extrato, saque e deposito
 
-print(BRAZILIAN_STATES)
+deposito: perguntar qual valor deseja depositar
 
+saque:    permitido realizar 3 saques diarios com limite max de 500 reais p saque, caso n tenha saldo em conta,
+          o sistema deve exibir uma msg informando que n é possivel sacar o dinheiro p falta de saldo.
 
-idade = float(idade)
+          todos os saques e depositos devem ser armazenados em suas variaveis e exibidos no extrato.
 
-print(idade)
-
-print(type(idade))
-print(type(nome))
-
-
-nome = input("informe seu nome:  ")
-idade = input("informe sua idade:  ")
-print(nome, idade)
-print(nome, idade, end="...\n")
-print(nome, idade, sep="#", end="...\n")
-print(nome, idade, sep="#")
+Extrato:  listar tds os dep e saques realizados na conta. no fim da listagem deve ser exibido o saldo atual da conta.
+          Os valores devem ser exibidos utilizando formato R$ xxxx.xx
 
 '''
 
-print(5//2)
+menu_conta = """
+   =========== MENU : CONTA CORRENTE =========
+
+   1 - Depositar
+   2 - Sacar
+   3 - Extrato
+   0 - Sair
+
+   ============================================
+
+   A seguir digite o Nº das opção do menu: => """
+saldo = 0
+limite_por_saque = 500
+extrato = ""
+numero_saques = 0
+limite_saques = 3
+
+
+
+while True:
+    opcao = int(input(menu_conta))
+
+    if opcao == 1:        
+      valor = float(input("\n   Entre com o valor a depositar: "))
+
+      if valor > 0:
+        saldo += valor
+        extrato += f"Depósito: R$ {valor:.2f}\n"
+        print(f"   seu saldo é de R$ {saldo:.2f}")
+      else:
+        print("   Operação inválida! o valor informado não é permitido.")
+
+    elif opcao == 2:
+       valor_saque = float(input("\n   Entre com o valor a sacar: "))
+       if numero_saques < 3:
+        if valor_saque <= saldo:
+          if valor_saque <= limite_por_saque:
+            saldo -= valor_saque
+            numero_saques += 1
+            extrato += f"Saque: R$ {valor_saque:.2f}\n"
+            print(f"   seu saldo é de R$ {saldo:.2f}")
+          else:
+           print(f"   Operação não permitida! Seu limite de saque é {limite_por_saque}.")
+        else:
+         print("   Você não tem saldo suficiente para esta operação!")
+         print(f"   seu saldo é de R$ {saldo:.2f}")
+       else:
+         print("   Operação não permitida! Você excedeu a quantidade de saques permitida!")
+    elif opcao == 3:
+        print("\n=====================  EXTRATO  ========================")
+        print("\n   Não houve movimentação em sua conta!" if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("\n========================================================")
+
+    elif opcao == 0:
+        print("   Obrigado por acessar nosso sistema!")
+        break
+    else:
+      print("   Operação inválida, por favor selecione outro valor do menu.")
