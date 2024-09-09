5886d87e3ac143e3b0792284ba9ec28c7d55c2f0
Geraldo Moreira <gera.moreira11@gmail.com>
2024-09-09 11:17:22 -0300

    projeto inicial sistema bancario

---
+++ bank_system.py
@@ -0,0 +1,29 @@
+'''
+nome = "geraldo"
+idade = 33
+nome = "caio"
+print(nome, idade)
+
+BRAZILIAN_STATES =["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
+
+
+idade = float(idade)
+
+print(idade)
+
+print(type(idade))
+print(type(nome))
+
+
+nome = input("informe seu nome:  ")
+idade = input("informe sua idade:  ")
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep="#")
+
+'''
+
+print(5//2)
